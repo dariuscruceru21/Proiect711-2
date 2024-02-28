df6e223d54329c5338c66ea85c14971eae913da5
nicoletaviregan <nico_viregan@yahoo.com>
2024-02-28 22:39:40 +0200

    restored main function code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Cruceru Darius" << std::endl;
-    std::cout << "Eminovici Robert" << std::endl;
-    std::cout << "Grama Andrei" << std::endl;
-    std::cout << "Gyarmati Philipp" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
