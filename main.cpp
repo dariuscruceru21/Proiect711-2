852d4e46b80a1c3d34c3676b3e366960d4c8b77a
OOP <roeminovici@gmail.com>
2024-02-28 23:38:18 +0200

    commit test2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+
+void test_func(){
+    std::cout << "Test Func" << std::endl;
+}
+
 int main() {
     std::cout << "Cruceru Darius" << std::endl;
     std::cout << "Eminovici Robert" << std::endl;
+    test_func();
     return 0;
 }
