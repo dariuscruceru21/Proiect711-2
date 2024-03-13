98b4188398673e83808b97706991bdaf13c9d4a3
dariuscruceru <dariuscruceru@gmail.com>
2024-03-13 18:38:22 +0200

    added members file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int test_func(int a, int b){
-    return a + b;
-}
 
 int main() {
 
-    std::cout << test_func(1, 2);
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
