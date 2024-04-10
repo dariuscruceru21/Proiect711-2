d51ba29d118b8662d996b8820b5b7bc223870d9d
andrafecske <andrafecske@gmail.com>
2024-04-10 18:50:42 +0300

    Created the representation for the client handling class

---
+++ Models/ClientHandling.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andra on 4/4/2024.
+//
+
+#include "ClientHandling.h"
---
+++ Models/ClientHandling.h
@@ -0,0 +1,35 @@
+//
+// Created by Andra on 4/4/2024.
+//
+#include <string>
+#ifndef PROIECT711_2_CLIENTHANDLING_H
+#define PROIECT711_2_CLIENTHANDLING_H
+
+
+class ClientHandling {
+    private:
+        int ID;
+        std :: string name, surname, email, phoneNumber, address, notes;
+        bool gdprDeleted;
+
+
+    public:
+        ClientHandling();
+
+        //aici sa se apeleze constructoru de la client
+        void createClient(int ID, std :: string name, std :: string  surname, std :: string email, std :: string  phoneNumber, std :: string  adress, std :: string  notes, bool gdprDeleted);
+
+
+        void changeClient(std :: string name = "", std :: string  surname = "", std :: string email = "", std :: string  phoneNumber = "", std :: string  adress = "", std :: string  notes = " ");
+
+        void deleteClient(int ID);
+
+        void listClients();
+
+        void searchClientsByEmail(std :: string email);
+        void searchClientsByName(std::string name, std::string surname);
+        void searchClientsByPhone(std::string phoneNumber);
+};
+
+
+#endif //PROIECT711_2_CLIENTHANDLING_H
