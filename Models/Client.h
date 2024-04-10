02bd2dab5883bd3fdd79bcefaa861d353b3b2488
dariuscruceru <dariuscruceru@gmail.com>
2024-04-10 18:59:02 +0300

    added Client class

---
+++ Models/Client.h
@@ -0,0 +1,21 @@
+#include "User.h"
+
+
+#ifndef PROIECT711_2_CLIENT_H
+#define PROIECT711_2_CLIENT_H
+
+
+class Client: public User{
+    private:
+        bool gdpr;
+        std::string telefonNumber;
+
+    public:
+        Client(std::string email, std::string password, std::string firstName, std::string lastName, std::string telefonNumber, bool gdpr);
+        Client();
+
+
+};
+
+
+#endif //PROIECT711_2_CLIENT_H
