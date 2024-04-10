2d1bd1d7f4c7cba5de0d834f19467e6ce7ea789d
scotiana2415 <scotiana2004@gmail.com>
2024-04-10 15:45:48 +0300

    added Admin class

---
+++ Models/Admin.cpp
@@ -0,0 +1,10 @@
+//
+// Created by scoti on 10.04.2024.
+//
+
+#include "Admin.h"
+
+Admin::Admin(std::string email, std::string password, std::string firstName, std::string lastName, std::string position, std::string birthDate, std::string initials,
+             double salary, std::string observations) {
+    Employee(email,password,firstName,lastName,position,birthDate,initials,salary,observations);
+}
