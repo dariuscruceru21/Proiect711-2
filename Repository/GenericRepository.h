634aad9a24a942d7742988976c123dbe8e186afb
dariuscruceru <dariuscruceru@gmail.com>
2024-04-17 16:33:39 +0300

    created a generic Repository to use for the other ones

---
+++ Repository/GenericRepository.h
@@ -0,0 +1,26 @@
+
+#ifndef PROIECT711_2_GENERICREPOSITORY_H
+#define PROIECT711_2_GENERICREPOSITORY_H
+
+#include "string"
+#include "vector"
+
+template<typename T>
+class GenericRepository {
+private:
+    std::string filename;
+
+public:
+    std::vector<T> elements;//this contains an array with elements of classes,thats why we use template
+
+    GenericRepository();
+    void readFile(std::string filename);
+    void deleteFromFile(std::string filename, int id);//deletes object with given id
+    void updateFile(std::string filename, int id);//updates object with goven id
+    void addToFile(std::string filename);//adds a new object
+
+
+};
+
+
+#endif
