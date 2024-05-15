bc20b2bfb638c5049670a5c7f1cb8b74e8eb81e2
dariuscruceru <dariuscruceru@gmail.com>
2024-05-15 19:20:21 +0300

    added setters + getters for employe class

--- Models/Employee.cpp
+++ Models/Employee.cpp
@@ -4,14 +4,56 @@
 
 #include "Employee.h"
 
-Employee::Employee(int id,std::string email, std::string password, std::string firstName, std::string lastName, std::string position, std::string birthDate, std::string initials, double salary, std::string observations){
+Employee::Employee(int id, std::string email, std::string password, std::string firstName, std::string lastName,
+                   std::string position, std::string birthDate, std::string initials, double salary,
+                   std::string observations) {
     this->position = position;
     this->birthDate = birthDate;
     this->initials = initials;
     this->salary = salary;
     this->observations = observations;
-    User(id,email, password, firstName, lastName);
+    User(id, email, password, firstName, lastName);
 
 }
 
 Employee::Employee() {}
+
+std::string Employee::getPosition() {
+    return this->position;
+}
+
+std::string Employee::getBirthDate() {
+    return this->birthDate;
+}
+
+std::string Employee::getInitials() {
+    return this->initials;
+}
+
+double Employee::getSalary() {
+    return this->salary;
+}
+
+std::string Employee::getObservation() {
+    return this->observations;
+}
+
+void Employee::setPosition(std::string newPosition) {
+    this->position = newPosition;
+}
+
+void Employee::setBirthDate(std::string newBirthdate) {
+    this->birthDate = newBirthdate;
+}
+
+void Employee::setInitials(std::string newInitials) {
+    this->initials = newInitials;
+}
+
+void Employee::setSalary(double newSalary) {
+    this->salary = newSalary;
+}
+
+void Employee::setObservation(std::string newObservation) {
+    this->observations = newObservation;
+}
--- Models/Employee.h
+++ Models/Employee.h
@@ -19,8 +19,31 @@ private:
 
 public:
 
-    Employee(int id,std::string email, std::string password, std::string firstName, std::string lastName, std::string position, std::string birthDate, std::string initials, double salary, std::string observations);
+    Employee(int id, std::string email, std::string password, std::string firstName, std::string lastName,
+             std::string position, std::string birthDate, std::string initials, double salary,
+             std::string observations);
+
     Employee();
+
+    std::string getPosition();
+
+    std::string getBirthDate();
+
+    std::string getInitials();
+
+    double getSalary();
+
+    std::string getObservation();
+
+    void setPosition(std::string newPosition);
+
+    void setBirthDate(std::string newBirthdate);
+
+    void setInitials(std::string newInitials);
+
+    void setSalary(double newSalary);
+
+    void setObservation(std::string newObservation);
 };
 
 #endif //PROIECT711_2_EMPLOYEE_H
