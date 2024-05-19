90845591ce1dfb13a9eab3447e560519f12d74d5
Philipp-Teodor Gyarmati <philipp.gyarmati@gmail.com>
2024-05-19 19:02:01 +0300

    Added B4.1, B4.2, B4.3 and a writeAll function for listing all details of the current order

--- Models/CarOrdering.h
+++ Models/CarOrdering.h
@@ -3,6 +3,7 @@
 
 #include <ctime>
 #include <iostream>
+#include <list>
 #include "User.h"
 #include "Car.h"
 //#include "CarHandling.h"
@@ -49,11 +50,22 @@ public:
     void setObservation(std::string observation);
     std::string getObservation();
 
+    //general function for writing an order's details
+    void writeAll();
 
     //B.2 Nur Mitarbeiter durfen
     int changeOrder(int orderNumber,Employee employee);
 
 
+    //B.4 Auflisten der Bestellungen
+    void showAllOrdersInASpecificTimeInterval(std::list<CarOrdering> repository, tm start, tm end);
+    static bool compareByTotalPrice(CarOrdering c1, CarOrdering c2)
+    { return c1.moneySum < c2.moneySum; }
+
+    CarOrdering searchOrderByOrderNumber(std::list<CarOrdering> repository, int orderNr);
+
+    void totalSumOfATimeInterval(std::list<CarOrdering> repository, tm time, std::string type); //time is either a month or a year (ex. January or 2024)
+                                                                                                //type is either month or year
 
 };
 
