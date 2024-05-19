7e673115e5429b58b964255b94ea3d70d9e53ef1
Paul D. <150134889+PolDC27@users.noreply.github.com>
2024-05-19 15:48:06 +0300

    Added setters+getters

--- Models/Client.cpp
+++ Models/Client.cpp
@@ -7,13 +7,52 @@
 
 
 Client::Client(int id,std::string email, std::string password, std::string firstName, std::string lastName,
-               std::string telefonNumber, bool gdpr) {
+               std::string telefonNumber, bool gdpr, std::string observations) {
     this->phoneNumber = telefonNumber;
     this->gdpr = gdpr;
-    User(id,email,password,firstName,lastName);
+    this->observations = observations;
+    User(id, email, password, firstName, lastName);
 }
+
 
 Client::Client() {}
 
 
+bool Client::getGdpr() const {
+    return this->gdpr;
+}
 
+
+std::string Client::getObservations() const {
+    return this->observations;
+}
+
+
+std::vector<std::string> Client::getFavoriteList() const {
+    return favoriteList;
+}
+
+
+std::string Client::getPhoneNumber() const {
+    return phoneNumber;
+}
+
+
+void Client::setObservations(std::string newObservations) {
+    observations = newObservations;
+}
+
+
+void Client::setGdpr(bool newGdpr) {
+    gdpr = newGdpr;
+}
+
+
+void Client::setPhoneNumber(std::string newNumber) {
+    phoneNumber = newNumber;
+}
+
+
+void Client::setFavoriteList(std::vector<std::string> newFavoritelist) {
+    favoriteList = newFavoritelist;
+}
